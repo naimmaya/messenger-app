b75df8473989b88240ade8f7372c452c7c4fe7b7
naimmaya <naimmaya130@gmail.com>
2026-09-21 02:31:04 +0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit, join_room
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'messenger_secret_key'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    emit('status', {'msg': f"{username} joined the chat."}, room=room)
+
+@socketio.on('send_message')
+def handle_send_message(data):
+    room = data['room']
+    emit('receive_message', {'sender': data['sender'], 'message': data['message']}, room=room)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
